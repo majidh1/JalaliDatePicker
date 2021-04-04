9d80ab5c21ce24be25f591d673682fd4ba0fba17
MAjid H1 <majidh1@live.com>
2021-04-04 11:45:47 +0430

    v0.2.1

--- src/utils.js
+++ src/utils.js
@@ -370,3 +370,21 @@ export const containsDom = (parent,event) => {
     }
     return path.indexOf(parent) !== -1;
 };
+
+export const getCustomEvent = (name) => {
+    (function () {
+        if (typeof window.CustomEvent === "function") return false; //If not IE
+
+        function CustomEvent(event, params) {
+            params = params || { bubbles: false, cancelable: false, detail: undefined };
+            const evt = document.createEvent("CustomEvent");
+            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+            return evt;
+        }
+
+        CustomEvent.prototype = window.Event.prototype;
+
+        window.CustomEvent = CustomEvent;
+    })();
+    return new CustomEvent(`${name}`, { "bubbles": true });
+};
